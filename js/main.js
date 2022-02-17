75ac99d4c6fcfe6d1b460da6b013e83db9d8c736
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-02-17 14:35:26 +0600

    main functions added

--- js/main.js
+++ js/main.js
@@ -32,6 +32,30 @@ document.getElementById("calculate-balance").addEventListener("click", function(
     const balanceShow = document.getElementById("balance");
     balanceShow.innerText = balance;
 
+})
 
+document.getElementById("saving-btn").addEventListener("click", function(){
+    // getting the income amount
+    const income = document.getElementById("income").value;
+    const incomeValue = parseInt(income);
 
+    // getting saving amount and setting them 
+    const savingAmount = document.getElementById("saving-amount").value;
+    const savingAmountValue = parseInt(savingAmount);
+
+    const savings = incomeValue - savingAmountValue;
+    
+    const savingTotal = document.getElementById("saving-total");
+    savingTotal.innerText = savings;
+
+    // remaining balance
+    //getting the previous balance
+    const previousBalance  = document.getElementById("balance").innerText;
+    const previousBalanceValue  = parseInt(previousBalance);
+
+    // calculating the remianing balance 
+    const remainingBalance = previousBalanceValue - savings;
+    //showing the remianing balance
+    const remainingBalanceShow = document.getElementById("remaining-balance");
+    remainingBalanceShow.innerText = remainingBalance;
 })
